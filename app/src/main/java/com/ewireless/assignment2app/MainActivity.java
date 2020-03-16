66ac828dd6c5c0b9eca8ccc710dba3b21f370489
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2020-03-16 16:01:13 +0000

    Setup activity transition API

--- app/src/main/java/com/ewireless/assignment2app/MainActivity.java
+++ app/src/main/java/com/ewireless/assignment2app/MainActivity.java
@@ -1,18 +1,79 @@
 package com.ewireless.assignment2app;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 
+import com.google.android.gms.location.ActivityRecognition;
+import com.google.android.gms.location.ActivityTransition;
+import com.google.android.gms.location.ActivityTransitionEvent;
+import com.google.android.gms.location.ActivityTransitionRequest;
+import com.google.android.gms.location.ActivityTransitionResult;
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.os.SystemClock;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    // Database reference initialisation
+    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+
+    private DatabaseReference mDatabaseReference = mDatabase.getReference().child("Accelerometer Data");
+
+    /***********Begin definitions for Activity API************/
+
+    // Tag for log prints
+    private final static String TAG = "MainActivity";
+
+    // Boolean for whether device is running API level 29 or later
+    // TODO: implement permission check
+    private boolean runningQOrLater = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q;
+
+    // Bool true if activity tracking is enabled
+    private boolean activityTrackingEnabled;
+
+    // List of activity transitions e.g. walking, still
+    private List<ActivityTransition> activityTransitionList;
+
+    // Action fired when transitions are triggered.
+    private final String TRANSITIONS_RECEIVER_ACTION =
+            BuildConfig.APPLICATION_ID + "TRANSITIONS_RECEIVER_ACTION";
+
+    private PendingIntent mActivityTransitionsPendingIntent;
+    private TransitionsReceiver mTransitionsReceiver;
+
+
+    /***********End definitions for Activity API************/
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +90,223 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Activity tracker is initially turned off
+        activityTrackingEnabled = false;
+
+        // List of activity transitions to track.
+        activityTransitionList = new ArrayList<>();
+
+        // Add the transitions we want to track to the array
+        // In this case we are tracking when we enter and exit walking and still states#
+        // The toActivity and toTransition methods handle string creation
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+                .build());
+        activityTransitionList.add(new ActivityTransition.Builder()
+                .setActivityType(DetectedActivity.WALKING)
+                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+                .build());
+
+        // Initialize PendingIntent that will be triggered when a activity transition occurs.
+        Intent intent = new Intent(TRANSITIONS_RECEIVER_ACTION);
+        mActivityTransitionsPendingIntent =
+                PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
+
+        //  Create and register a BroadcastReceiver to listen for activity transitions.
+        // The receiver listens for the PendingIntent above that is triggered by the system when an
+        // activity transition occurs.
+        mTransitionsReceiver = new TransitionsReceiver();
+        //LocalBroadcastManager.getInstance(this).registerReceiver(
+        registerReceiver(
+                //this.registerReceiver(
+                mTransitionsReceiver,
+                new IntentFilter(TRANSITIONS_RECEIVER_ACTION)
+        );
+
+        Log.d(TAG, "App initialized.");
     }
 
 
+    @Override
+    protected void onPause() {
+        /*
+        // Disable activity transitions when user leaves the app.
+        if (activityTrackingEnabled) {
+            disableActivityTransitions();
+        }*/
+        super.onPause();
+    }
+
+    // Perform appropriate action when the user leaves (but does not destroy) the app
+    @Override
+    protected void onStop() {
+
+
+        super.onStop();
+    }
+
+    // Unregister receivers which should only stop when the app is destroyed
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Unregister activity transition receiver when user destroys the app.
+        if (mTransitionsReceiver != null) {
+            LocalBroadcastManager.getInstance(this).unregisterReceiver(mTransitionsReceiver);
+            mTransitionsReceiver = null;
+        }
+    }
+
+    /***********Begin Activity API methods************/
+
+    // handles list creation
+    private static String toActivityString(int activity) {
+        switch (activity) {
+            case DetectedActivity.WALKING:
+                return "WALKING";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+    private static String toTransitionType(int transitionType) {
+        switch (transitionType) {
+            case ActivityTransition.ACTIVITY_TRANSITION_ENTER:
+                return "ENTER";
+            case ActivityTransition.ACTIVITY_TRANSITION_EXIT:
+                return "EXIT";
+            default:
+                return "UNKNOWN";
+        }
+    }
+
+
+
+    /**
+     * Registers callbacks for {@link ActivityTransition} events via a custom
+     * {@link BroadcastReceiver}
+     */
+    private void enableActivityTransitions() {
+
+        Log.d(TAG, "enableActivityTransitions()");
+
+
+        // TODO: Create request and listen for activity changes.
+        ActivityTransitionRequest request = new ActivityTransitionRequest(activityTransitionList);
+
+        // Register for Transitions Updates.
+        Task<Void> task =
+                ActivityRecognition.getClient(this)
+                        .requestActivityTransitionUpdates(request, mActivityTransitionsPendingIntent);
+
+
+        task.addOnSuccessListener(
+                new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void result) {
+                        //activityTrackingEnabled = true;
+                        Log.d(TAG, "Enable Complete");
+                    }
+                });
+        task.addOnFailureListener(
+                new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "Enable Failed");
+                    }
+                });
+
+
+    }
+
+    /**
+     * Unregisters callbacks for {@link ActivityTransition} events via a custom
+     * {@link BroadcastReceiver}
+     */
+    private void disableActivityTransitions() {
+
+        Log.d(TAG, "disableActivityTransitions()");
+
+
+        // TODO: Stop listening for activity changes.
+        ActivityRecognition.getClient(this).removeActivityTransitionUpdates(mActivityTransitionsPendingIntent)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        activityTrackingEnabled = false;
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
+        Log.d(TAG, "Disable Complete");
+    }
+
+    public void onClickEnableOrDisableActivityRecognition(View view) {
+
+        // TODO: Add requests for activity tracking.
+        if (activityTrackingEnabled) {
+            disableActivityTransitions();
+
+        } else {
+            enableActivityTransitions();
+        }
+
+    }
+
+    /**
+     * Handles intents from from the Transitions API.
+     */
+    public class TransitionsReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Log.d(TAG, "onReceive Called");
+
+            if (!TextUtils.equals(TRANSITIONS_RECEIVER_ACTION, intent.getAction())) {
+
+                return;
+            }
+
+            // Extract activity transition information from listener.
+            if (ActivityTransitionResult.hasResult(intent)) {
+
+                ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
+
+                for (ActivityTransitionEvent event : result.getTransitionEvents()) {
+                    //continue only if the activity happened in the last 30 seconds
+                    //for some reason callbacks are received for old activities when the receiver is registered
+                    if(((SystemClock.elapsedRealtime()-(event.getElapsedRealTimeNanos()/1000000))/1000) <= 30) {
+                        //activity transition is legit. Do stuff here.
+                        transitionHandler(event.getActivityType(), event.getTransitionType());
+                    }
+
+                }
+            }
+        }
+    }
+
+    // Decides what to do when a transition occurs
+    // If entering walk then start gaitAnalysis service and write cadence data to file
+    // If exiting walk end gaitAnalysis service and upload data to database
+    private void transitionHandler(int activityType, int transitionType) {
+        String info = "Transition: " + toActivityString(activityType) +
+                " (" + toTransitionType(transitionType) + ")" + "   " +
+                new SimpleDateFormat("HH:mm:ss", Locale.UK).format(new Date());
+
+        Log.d(TAG, info);
+        DatabaseReference newRef = mDatabaseReference.push();
+        newRef.setValue(info);
+    }
+
+
+
+    /***********End Activity API methods************/
 
 
 
