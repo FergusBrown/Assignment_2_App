26c1124372f7f7c6ef4f887ebf7d4f7a170067aa
FergusBrown <43826955+FergusBrown@users.noreply.github.com>
2020-03-17 13:52:27 +0000

    Activity transition API moved to a service

--- app/src/main/java/com/ewireless/assignment2app/MainActivity.java
+++ app/src/main/java/com/ewireless/assignment2app/MainActivity.java
@@ -44,9 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     // Database reference initialisation
-    FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+    //FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
 
-    private DatabaseReference mDatabaseReference = mDatabase.getReference().child("Activity Data");
+    //private DatabaseReference mDatabaseReference = mDatabase.getReference().child("Activity Data");
 
     /***********Begin definitions for Activity API************/
 
@@ -57,325 +57,52 @@ public class MainActivity extends AppCompatActivity {
     // TODO: implement permission check
     private boolean runningQOrLater = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q;
 
-    // Bool true if activity tracking is enabled
-    private boolean activityTrackingEnabled;
 
-    // List of activity transitions e.g. walking, still
-    private List<ActivityTransition> activityTransitionList;
-
-    // Action fired when transitions are triggered.
-    private final String TRANSITIONS_RECEIVER_ACTION =
-            BuildConfig.APPLICATION_ID + "TRANSITIONS_RECEIVER_ACTION";
-
-    private PendingIntent mActivityTransitionsPendingIntent;
-    private TransitionsReceiver mTransitionsReceiver;
-
+    // Intent for the activity service
+    Intent activityService;
 
     /***********End definitions for Activity API************/
 
 
-    TextView status;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        status = findViewById(R.id.status);
+        //status = findViewById(R.id.status);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        activityService = new Intent(this, ActivityRecognitionService.class);
 
-        // Activity tracker is initially turned off
-        activityTrackingEnabled = false;
+        startService(activityService);
 
-        // List of activity transitions to track.
-        activityTransitionList = new ArrayList<>();
-
-        // Add the transitions we want to track to the array
-        // In this case we are tracking when we enter and exit walking and still states#
-        // The toActivity and toTransition methods handle string creation
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.WALKING)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-                .build());
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.WALKING)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                .build());
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.STILL)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-                .build());
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.STILL)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                .build());
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.ON_BICYCLE)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-                .build());
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.ON_BICYCLE)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                .build());
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.IN_VEHICLE)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-                .build());
-        activityTransitionList.add(new ActivityTransition.Builder()
-                .setActivityType(DetectedActivity.IN_VEHICLE)
-                .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-                .build());
-
-        // Initialize PendingIntent that will be triggered when a activity transition occurs.
-        Intent intent = new Intent(TRANSITIONS_RECEIVER_ACTION);
-        mActivityTransitionsPendingIntent =
-                PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
-
-        //  Create and register a BroadcastReceiver to listen for activity transitions.
-        // The receiver listens for the PendingIntent above that is triggered by the system when an
-        // activity transition occurs.
-        mTransitionsReceiver = new TransitionsReceiver();
-        //LocalBroadcastManager.getInstance(this).registerReceiver(
-        registerReceiver(
-                //this.registerReceiver(
-                mTransitionsReceiver,
-                new IntentFilter(TRANSITIONS_RECEIVER_ACTION)
-        );
-
-
-        //enableActivityTransitions();
         Log.d(TAG, "App initialized.");
     }
 
 
     @Override
     protected void onResume() {
-        enableActivityTransitions();
         super.onResume();
     }
 
     @Override
     protected void onPause() {
-        /*
-        // Disable activity transitions when user leaves the app.
-        if (activityTrackingEnabled) {
-            disableActivityTransitions();
-        }*/
         super.onPause();
     }
 
     // Perform appropriate action when the user leaves (but does not destroy) the app
     @Override
     protected void onStop() {
-
-
         super.onStop();
     }
 
     // Unregister receivers which should only stop when the app is destroyed
     @Override
     protected void onDestroy() {
+        // End all services
+        stopService(activityService);
         super.onDestroy();
-
-        // Unregister activity transition receiver when user destroys the app.
-        if (mTransitionsReceiver != null) {
-            LocalBroadcastManager.getInstance(this).unregisterReceiver(mTransitionsReceiver);
-            mTransitionsReceiver = null;
-        }
     }
-
-    /***********Begin Activity API methods************/
-
-    // handles list creation
-    private static String toActivityString(int activity) {
-        switch (activity) {
-            case DetectedActivity.STILL:
-                return "STILL";
-            case DetectedActivity.WALKING:
-                return "WALKING";
-            case DetectedActivity.RUNNING:
-                return "RUNNING";
-            case DetectedActivity.IN_VEHICLE:
-                return "IN VEHICLE";
-            case DetectedActivity.ON_BICYCLE:
-                return "ON BICYCLE";
-            default:
-                return "UNKNOWN";
-        }
-    }
-
-    private static String toTransitionType(int transitionType) {
-        switch (transitionType) {
-            case ActivityTransition.ACTIVITY_TRANSITION_ENTER:
-                return "ENTER";
-            case ActivityTransition.ACTIVITY_TRANSITION_EXIT:
-                return "EXIT";
-            default:
-                return "UNKNOWN";
-        }
-    }
-
-
-
-    /**
-     * Registers callbacks for {@link ActivityTransition} events via a custom
-     * {@link BroadcastReceiver}
-     */
-    private void enableActivityTransitions() {
-
-        Log.d(TAG, "enableActivityTransitions()");
-
-
-        // TODO: Create request and listen for activity changes.
-        ActivityTransitionRequest request = new ActivityTransitionRequest(activityTransitionList);
-
-        // Register for Transitions Updates.
-        Task<Void> task =
-                ActivityRecognition.getClient(this)
-                        .requestActivityTransitionUpdates(request, mActivityTransitionsPendingIntent);
-
-
-        task.addOnSuccessListener(
-                new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void result) {
-                        activityTrackingEnabled = true;
-                        Log.d(TAG, "Enable Complete");
-                    }
-                });
-        task.addOnFailureListener(
-                new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "Enable Failed");
-                    }
-                });
-
-
-    }
-
-    /**
-     * Unregisters callbacks for {@link ActivityTransition} events via a custom
-     * {@link BroadcastReceiver}
-     */
-    private void disableActivityTransitions() {
-
-        Log.d(TAG, "disableActivityTransitions()");
-
-
-        // TODO: Stop listening for activity changes.
-        ActivityRecognition.getClient(this).removeActivityTransitionUpdates(mActivityTransitionsPendingIntent)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        activityTrackingEnabled = false;
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                    }
-                });
-        Log.d(TAG, "Disable Complete");
-    }
-
-    public void onClickEnableOrDisableActivityRecognition(View view) {
-
-        // TODO: Add requests for activity tracking.
-        if (activityTrackingEnabled) {
-            disableActivityTransitions();
-
-        } else {
-            enableActivityTransitions();
-        }
-
-    }
-
-    /**
-     * Handles intents from from the Transitions API.
-     */
-    public class TransitionsReceiver extends BroadcastReceiver {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String activityType, transitionType;
-
-
-            Log.d(TAG, "onReceive Called");
-
-            if (!TextUtils.equals(TRANSITIONS_RECEIVER_ACTION, intent.getAction())) {
-
-                return;
-            }
-
-            // Extract activity transition information from listener.
-            if (ActivityTransitionResult.hasResult(intent)) {
-
-                ActivityTransitionResult result = ActivityTransitionResult.extractResult(intent);
-
-                for (ActivityTransitionEvent event : result.getTransitionEvents()) {
-                    activityType = toActivityString(event.getActivityType());
-                    transitionType = toTransitionType(event.getTransitionType());
-                    //continue only if the activity happened in the last 30 seconds
-                    //for some reason callbacks are received for old activities when the receiver is registered
-                    if(((SystemClock.elapsedRealtime()-(event.getElapsedRealTimeNanos()/1000000))/1000) <= 30) {
-                        //activity transition is legit. Do stuff here.
-                        transitionHandler(activityType, transitionType);
-                    }
-
-                }
-            }
-        }
-    }
-
-    // Decides what to do when a transition occurs
-    // If entering walk then start gaitAnalysis service and write cadence data to file
-    // If exiting walk end gaitAnalysis service and upload data to database
-    private void transitionHandler(String activityType, String transitionType) {
-        String info = "Transition: " + activityType +
-                " (" + transitionType + ")" + "   " +
-                new SimpleDateFormat("HH:mm:ss", Locale.UK).format(new Date());
-
-        Log.d(TAG, info);
-        DatabaseReference newRef = mDatabaseReference.push();
-        newRef.setValue(info);
-    }
-
-
-
-    /***********End Activity API methods************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
