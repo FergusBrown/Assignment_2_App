5970dfdb3b6213c4a220768610b33411674e931d
roland0000 <62400812+roland0000@users.noreply.github.com>
2020-04-09 14:33:58 +0100

    Add files via upload

--- fd/app/src/main/java/com/example/falldetection/MainActivity.java
+++ fd/app/src/main/java/com/example/falldetection/MainActivity.java
@@ -40,13 +40,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sm;
     private Sensor aSensor;
     private Sensor gSensor;
+    private Sensor mSensor;
 
     float[] accelerometerValues = new float[3];
     float[] gyroscopeValues = new float[3];
+    float[] magnetometerValues = new float[3];
     double aThreshold;
     double gThreshold;
+    double mThreshold;
     double aMax;
     double gMax;
+    double aMax0;
+    double aMax2;
+    double tMin1 = 90;
+    double tMin2 = 90;
+    double theta1;
+    double theta2;
+    double degree;
     long time;
     double ayMin;
     double ayMax;
@@ -67,22 +77,54 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         tvAcceleration = (TextView)findViewById(R.id.tvAcceleration);
         tvRotation = (TextView)findViewById(R.id.tvRotation);
 
+        f = 0;
+
         sm = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         aSensor = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         gSensor = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mSensor = sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
     }
 
     private void calculateOrientation() {
         aThreshold = Math.sqrt(accelerometerValues[0]*accelerometerValues[0] + accelerometerValues[1]*accelerometerValues[1] + accelerometerValues[2]*accelerometerValues[2]);
         gThreshold = Math.sqrt(gyroscopeValues[0]*gyroscopeValues[0] + gyroscopeValues[1]*gyroscopeValues[1] + gyroscopeValues[2]*gyroscopeValues[2]);
+        theta1 = Math.toDegrees(Math.atan((Math.sqrt(accelerometerValues[1]*accelerometerValues[1] + accelerometerValues[2]*accelerometerValues[2]))/(accelerometerValues[0])));
+        theta2 = Math.toDegrees(Math.atan((Math.sqrt(accelerometerValues[1]*accelerometerValues[1] + accelerometerValues[0]*accelerometerValues[0]))/(accelerometerValues[2])));
 
-        if (aThreshold > aMax) aMax = aThreshold;
-        if (gThreshold > gMax) gMax = gThreshold;
+        float[] values = new float[3];
+        float[] R = new float[9];
+
+        SensorManager.getRotationMatrix(R, null, accelerometerValues, magnetometerValues);
+        SensorManager.getOrientation(R, values);
+
+        degree = (float) Math.toDegrees(values[2]);
+
+        //if (theta > aMax) aMax = theta;
+        //if (aThreshold < gMax) gMax = gThreshold;
 
         tvAcceleration.setText("Acceleration: " + aThreshold);
-        tvRotation.setText("Rotation: " + gThreshold);
+        //tvRotation.setText("Rotation: " + gThreshold);
 
-        if (aThreshold > 20 && time == 0) {
+        if(f < 1000){
+            f++;
+            if(accelerometerValues[0] > aMax0) aMax0 = accelerometerValues[0];
+            if(accelerometerValues[2] > aMax2) aMax2 = accelerometerValues[2];
+            if(Math.abs(theta1) < tMin1) tMin1 = Math.abs(theta1);
+            if(Math.abs(theta2) < tMin2) tMin2 = Math.abs(theta2);
+            if(aMax0 > 18 && aMax2 > 18 && (tMin1 < 45 || tMin2 < 45)){
+                Toast.makeText(this, "Fall registered", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, CDTimer.class));
+            }
+        }
+        else{
+            f = 0;
+            aMax0 = 0;
+            aMax2 = 0;
+            tMin1 = 90;
+            tMin2 = 90;
+        }
+
+        /*if (aThreshold > 20 && time == 0) {
             //Toast.makeText(this, "Fall check 1", Toast.LENGTH_SHORT).show();
             //aMax = 0;
             //gMax = 0;
@@ -112,7 +154,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Toast.makeText(this, "Fall registered", Toast.LENGTH_SHORT).show();
             f = 0;
             startActivity(new Intent(MainActivity.this, CDTimer.class));
-        }
+        }*/
     }
 
     @Override
@@ -120,6 +162,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         sm.registerListener(this, aSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sm.registerListener(this, gSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        sm.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
@@ -135,6 +178,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
         if(event.sensor.getType()==Sensor.TYPE_GYROSCOPE){
             gyroscopeValues = event.values;
+        }
+        if(event.sensor.getType()==Sensor.TYPE_MAGNETIC_FIELD){
+            magnetometerValues = event.values;
         }
         calculateOrientation();
     }
